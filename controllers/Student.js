dfd8b2e7ab7c69789d8a5fa16c1d25ff9e8d3bdd
Bhargav-kargatiya <bhargav.k1@ahduni.edu.in>
2024-08-16 12:16:18 +0530

    student can view their tasks

--- controllers/Student.js
+++ controllers/Student.js
@@ -1,3 +1,4 @@
+import Classroom from "../models/Classroom.js";
 import Student from "../models/Student.js";
 import asyncHandler from "express-async-handler";
 
@@ -31,21 +32,45 @@ export const createStudent = asyncHandler(async (req, res) => {
 export const getStudentClassrooms = asyncHandler(async (req, res) => {
     try {
         const { studentId } = req.params;
-
-        // Find the student and populate the classrooms field
         const student = await Student.findById(studentId).populate('classrooms');
         if (!student) {
             return res.status(404).json({ message: 'Student not found' });
         }
-
-        // Map the populated classrooms to the desired response format
         const classrooms = student.classrooms.map(classroom => ({
             classroomId: classroom._id.toString(),
             classroomName: classroom.name
         }));
-
-        // Return the list of classrooms
         res.status(200).json(classrooms);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+export const getStudentClassroomTasks = asyncHandler(async (req, res) => {
+    try {
+        const { studentId, classroomId } = req.params;
+        const student = await Student.findById(studentId).populate({
+            path: 'classrooms',
+            match: { _id: classroomId },
+            populate: 'tasks'
+        });
+
+        if (!student) {
+            return res.status(404).json({ message: 'Student not found' });
+        }
+
+        const classroom = student.classrooms[0];
+        if (!classroom) {
+            return res.status(404).json({ message: 'Classroom not found or student is not enrolled' });
+        }
+        const tasks = classroom.tasks.map(task => ({
+            taskId: task._id.toString(),
+            title: task.title,
+            description: task.description,
+            dueDate: task.dueDate.toISOString().split('T')[0]
+        }));
+        res.status(200).json(tasks);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal server error' });
--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createStudent, getStudentClassrooms } from "../controllers/Student.js";
+import { createStudent, getStudentClassrooms, getStudentClassroomTasks } from "../controllers/Student.js";
 
 
 
 const studentRoutes = express.Router();
 studentRoutes.post('/create', createStudent);
 studentRoutes.get('/:studentId/classrooms', getStudentClassrooms);
+studentRoutes.get('/:studentId/classrooms/:classroomId/tasks', getStudentClassroomTasks);
 
 
 
